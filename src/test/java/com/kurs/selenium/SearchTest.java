00e869e483fe149423b856d4c9aa3da08e0fb18e
Magdalena Pawłowska <pawlowskam123@gmail.com>
2019-09-06 19:46:34 +0200

    Initial commit

---
+++ src/test/java/com/kurs/selenium/SearchTest.java
@@ -0,0 +1,49 @@
+package com.kurs.selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.junit.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SearchTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+
+
+        System.setProperty("webdriver.gecko.driver",
+
+                "src/test/resources/geckodriver");
+
+
+        // Uruchom nowy egzemplarz przeglądarki Chrome
+        driver = new FirefoxDriver();
+
+        // Zmaksymalizuj okno przeglądarki
+        driver.manage().window().maximize();
+        // Przejdź do Google
+        driver.get("http://www.google.com");
+    }
+    @Test
+    public void testGoogleSearch() {
+        // Znajdź element wprowadzania tekstu na podstawie jego nazwy
+        WebElement element = driver.findElement(By.name("q"));
+        // Wyczyść teskst zapisany w elemencie
+        element.clear();
+
+        // Wpisz informacje do wyszukania
+        element.sendKeys("Testowanie Selenium WebDriver");
+
+        // Prześlij formularz
+        element.submit();
+    }
+    @After
+    public void tearDown() throws Exception {
+        // Zamknij przeglądarkę
+        driver.quit();
+    }
+}
